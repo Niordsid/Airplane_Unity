6cdbf201757c955b84f3ef3c6bcd7ee4cf76b49c
Andres Santana <andriy_99@hotmail.com>
2016-12-16 19:07:19 -0500

    Se crea el repositorio para el game

---
+++ Assets/Project/Script/AirplaneControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneControl : MonoBehaviour {
+
+
+	public float planespeed = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		// Mover a la derecha es insertar un valor positivo en X , dependiendo de la ubicacion de la camara
+		// Mover a la izquierda es insertar un valor negativo en X , dependiendo de la ubicacion de la camara
+
+		// Mover hacia arriba es insertar un valor positivo en Y
+		// Mover hacia abajo es insertar un valor negativo en Y
+		if (Input.GetKey ("left") == true){
+
+			Vector3 delta = new Vector3 (- planespeed, 0, 0);   // Los valores decimales se escriben agregando la f = float
+
+			gameObject.transform.Translate (delta);
+		}
+
+		if (Input.GetKey ("right")== true){
+
+			Vector3 delta = new Vector3 (planespeed, 0, 0);   // Los valores decimales se escriben agregando la f = float
+
+			gameObject.transform.Translate (delta);
+		}
+
+		if (Input.GetKey ("up") == true){
+
+			Vector3 delta = new Vector3 (0, planespeed, 0);   // Los valores decimales se escriben agregando la f = float
+
+			gameObject.transform.Translate (delta);
+		}
+
+		if (Input.GetKey ("down") == true){
+
+			Vector3 delta = new Vector3 (0, - planespeed, 0);   // Los valores decimales se escriben agregando la f = float
+
+			gameObject.transform.Translate (delta);
+		}
+
+	}
+}
---
+++ Assets/Project/Script/MissileLaunch.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileLaunch : MonoBehaviour {
+
+	public GameObject hellfire;
+	public GameObject fire;
+	public GameObject nextParent;
+	public AudioSource shoot; 
+
+	bool waslaunched = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (Input.GetKey ("space") == true) {
+			ParticleSystem llamas = fire.GetComponent<ParticleSystem> ();
+
+			// activar las llamas
+			llamas.Play ();
+
+			//ASignamos el misisl como hijo deñl nuevo padre
+			//para evitar 
+			gameObject.transform.SetParent(nextParent.transform);
+
+			waslaunched = true;
+		}
+
+		if  (waslaunched == true){
+
+			Vector3 delta = new Vector3 (0, 0, 1f);   // Los valores decimales se escriben agregando la f = float
+			gameObject.transform.Translate(delta);
+
+		}
+
+	}
+}
